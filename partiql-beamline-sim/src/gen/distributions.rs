ab93c736db3aadd1cb66c2505e8760b07e7fed1e
Josh Pschorr <joshps@amazon.com>
2024-06-08 13:36:17 -0700

    Refactor reader into a module.

--- partiql-beamline-sim/src/gen/distributions.rs
+++ partiql-beamline-sim/src/gen/distributions.rs
@@ -7,7 +7,6 @@ use rand_distr::Distribution;
 use statrs::distribution::Categorical;
 use std::cell::RefCell;
 use std::fmt::{Debug, Formatter};
-use std::marker::PhantomData;
 use std::ops::DerefMut;
 
 #[derive(Copy, Clone, Debug)]
@@ -171,75 +170,5 @@ where
 
     fn value_type(&self) -> PartiqlType {
         self.inner.value_type()
-    }
-}
-
-pub struct SimpleRandomVariableImpl<R, F>
-where
-    R: Rng + Sized + Clone,
-    F: Fn(&mut R, &SimContext) -> Value,
-{
-    pub(crate) name: String,
-    pub(crate) typ: PartiqlType,
-    pub(crate) f: F,
-    rng: PhantomData<R>,
-}
-
-pub type SimpleRandomVariable<R, F> = RandomVariable<R, SimpleRandomVariableImpl<R, F>>;
-
-impl<R, F> SimpleRandomVariable<R, F>
-where
-    R: Rng + Sized + Clone,
-    F: Fn(&mut R, &SimContext) -> Value + Clone,
-{
-    pub fn new(rng: R, name: String, typ: PartiqlType, f: F) -> DataGenerationResult<Self> {
-        let inner = SimpleRandomVariableImpl {
-            name,
-            typ,
-            f,
-            rng: PhantomData,
-        };
-        RandomVariable::create(rng, inner)
-    }
-}
-
-impl<R, F> Clone for SimpleRandomVariableImpl<R, F>
-where
-    R: Rng + Sized + Clone,
-    F: Fn(&mut R, &SimContext) -> Value + Clone,
-{
-    fn clone(&self) -> Self {
-        Self {
-            name: self.name.clone(),
-            typ: self.typ.clone(),
-            f: self.f.clone(),
-            rng: self.rng,
-        }
-    }
-}
-
-impl<R, F> Debug for SimpleRandomVariableImpl<R, F>
-where
-    R: Rng + Sized + Clone,
-    F: Fn(&mut R, &SimContext) -> Value,
-{
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("SimpleRandomVariable")
-            .field("name", &self.name)
-            .finish()
-    }
-}
-
-impl<R, F> InnerValueGenerator<R> for SimpleRandomVariableImpl<R, F>
-where
-    R: Rng + Sized + Clone,
-    F: Fn(&mut R, &SimContext) -> Value + Clone,
-{
-    fn present_value(&self, rng: &mut R, ctx: &SimContext) -> Value {
-        (self.f)(rng, ctx)
-    }
-
-    fn value_type(&self) -> PartiqlType {
-        self.typ.clone()
     }
 }
